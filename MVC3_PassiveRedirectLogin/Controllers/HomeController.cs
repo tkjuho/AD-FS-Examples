947911e81a4bf5a56dbe86340882ce8a88cc4e9f
Tomi Juhola <tkjuho@gmail.com>
2011-10-12 07:19:49 +0300

    Finished first example

--- MVC3_PassiveRedirectLogin/Controllers/HomeController.cs
+++ MVC3_PassiveRedirectLogin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.IdentityModel.Claims;
 
 namespace MVC3_PassiveRedirectLogin.Controllers
 {
@@ -10,10 +11,18 @@ namespace MVC3_PassiveRedirectLogin.Controllers
     {
         //
         // GET: /Home/
-
         public ActionResult Index()
         {
-            return View();
+            var claimsPrincipal = System.Threading.Thread.CurrentPrincipal as IClaimsPrincipal;
+            var claimsIdentity = (IClaimsIdentity)claimsPrincipal.Identity;
+
+            var pairs = new Dictionary<string, string>();
+            foreach (Claim claim in claimsIdentity.Claims)
+            {
+                pairs.Add(claim.ClaimType, claim.Value);
+            }
+
+            return View(pairs);
         }
 
     }
